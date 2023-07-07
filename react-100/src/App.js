a3d22dbacce2081ed1d5cabd385943702463e1a8
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-07-06 18:53:41 -0600

    feat: export-import lesson 9-10

--- react-100/src/App.js
+++ react-100/src/App.js
@@ -1,35 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Tech from './Tech';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Tech />
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">News</a>
-      <a href="#">About</a>
-    </div>
-  );
-};
-
-const Tech = () => {
-  return (
-    <div>
-      <h1>Hello React!</h1>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>React</li>
-        <li>TS</li>
-      </ul>
     </div>
   );
 };
---
+++ react-100/src/Tech.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Tech = () => {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>TS</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Tech;
