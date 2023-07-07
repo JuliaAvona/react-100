94d55ef199471b03567e5699ada0a255f58cf985
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-07-07 00:18:52 -0600

    feat: lesson 12

---
+++ react-100/src/components/Content.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNkHJrb01ivtcr8JPllzt8md3UgQ6Iy41S8QT0GvgV4Cobb0fjORsUVaHO5gsfgYERPmI&usqp=CAU"
+          alt="wallpaper"
+        />
+      </div>
+      <div>
+        <img
+          src="https://www.datocms-assets.com/89231/1682439036-ava.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Content;
---
+++ react-100/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="logo"
+        src="https://cryptologos.cc/logos/uniswap-uni-logo.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
