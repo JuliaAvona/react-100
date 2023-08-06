e67638aa5b9b354b85d944786d5d047eb4ba9a0e
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-08-05 21:35:14 -0500

    feat: 21. React JS Lessons (Laying Out the Dialog Page)

--- react-100/src/components/Dialogs/Dialogs.jsx
+++ react-100/src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
-//import classes from './Dialogs.module.css';
+import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogItems}>
+        <div className={styles.dialog + ' ' + styles.active}>Ivan</div>
+        <div className={styles.dialog}>Petr</div>
+        <div className={styles.dialog}>Maria</div>
+        <div className={styles.dialog}>Karl</div>
+        <div className={styles.dialog}>Mark</div>
+        <div className={styles.dialog}>Zar</div>
+      </div>
+      <div className={styles.messageItems}>
+        <div className={styles.message}>Hello</div>
+        <div className={styles.message}>How are you?</div>
+        <div className={styles.message}>I am fine</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
