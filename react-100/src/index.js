a2554980191250f749725454b3deef98507ad738
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-08-18 19:50:44 -0600

    feat: 37. Lecciones, Course React JS - store, state, OOP, refactoring

--- react-100/src/index.js
+++ react-100/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, { addPost, updateNewPostText, subscribe } from './redux/state';
+import store from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,16 +11,16 @@ const rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
       <App
-        state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        state={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />
     </React.StrictMode>
   );
 };
 
-subscribe(rerenderEntireTree);
-rerenderEntireTree(state);
+store.subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
 reportWebVitals();
 
 export default rerenderEntireTree;
--- react-100/src/redux/state.js
+++ react-100/src/redux/state.js
@@ -1,57 +1,59 @@
-let rerenderEntireTree = () => {
-  console.log('Rerendering entire tree');
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-      { id: 2, message: "It's my first post", likesCount: 11 },
-      { id: 3, message: 'Blabla', likesCount: 11 },
-      { id: 4, message: 'Dada', likesCount: 11 },
-    ],
-    newPostText: 'Hola mundo',
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+        { id: 2, message: "It's my first post", likesCount: 11 },
+        { id: 3, message: 'Blabla', likesCount: 11 },
+        { id: 4, message: 'Dada', likesCount: 11 },
+      ],
+      newPostText: 'Hola mundo!',
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrew' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Viktor' },
+        { id: 6, name: 'Valera' },
+      ],
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is your it-kamasutra?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' },
+      ],
+    },
+    sidebar: {},
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Dimych' },
-      { id: 2, name: 'Andrew' },
-      { id: 3, name: 'Sveta' },
-      { id: 4, name: 'Sasha' },
-      { id: 5, name: 'Viktor' },
-      { id: 6, name: 'Valera' },
-    ],
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'How is your it-kamasutra?' },
-      { id: 3, message: 'Yo' },
-      { id: 4, message: 'Yo' },
-      { id: 5, message: 'Yo' },
-    ],
+  getState() {
+    debugger;
+    return this._state;
   },
-  sidebar: {},
+  _callSubscriber() {
+    console.log('State changed');
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer; // observer
+  },
 };
 
-window.state = state;
+window.store = store;
 
-export let addPost = (postMessage) => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree();
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
